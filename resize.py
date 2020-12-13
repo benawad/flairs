1c2fd7e4cb4c3279d01fbbf338f8ce5d26f52e81
Ben Awad <benawad97@gmail.com>
2020-12-12 21:48:57 -0600

    mapjs

--- resize.py
+++ resize.py
@@ -1,11 +1,20 @@
 from PIL import Image
+import json
 import os
 
-flairs = os.listdir('raw/')
+flairs = os.listdir("raw/")
 
 for flair in flairs:
-    im = Image.open('raw/' + flair)
-    im.resize((52, 52)).save('resized/' + flair)
+    im = Image.open("raw/" + flair)
+    im.resize((52, 52)).save("resized/" + flair)
 
-with open('flairs.txt', 'w') as f:
-    f.write('\n'.join(sorted(flairs)))
+resized_flairs = os.listdir("resized/")
+
+with open("flairs.txt", "w") as f:
+    f.write("\n".join(sorted(resized_flairs)))
+
+with open("map.js", "w") as f:
+    d = {}
+    for flair in resized_flairs:
+        d[flair.split(".")[0]] = flair
+    f.write("export const flairMap = " + json.dumps(d))
